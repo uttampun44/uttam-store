4edb99ed0d171c1a1b2c5f307e09b787dde6cbc3
uttam pun <uttampun62@gmail.com>
2023-03-30 07:58:55 +0545

    footer added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+   <Footer >
+         <div className='footer-name'>
+                <div className='footer-menu'>
+                     <div className='uttam-store-footer'>
+                          <h5>Uttam Store</h5>
+                          <div className='subscribe'>
+                              <p>Subscribe</p>
+                          </div>
+                     </div>
+
+                     <div className=''>
+
+                     </div>
+                </div>
+         </div>
+   </Footer>
   )
 }
 
