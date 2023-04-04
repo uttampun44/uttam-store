487763df178f9ab778dcee52e5d9aa234a43dd29
uttam pun <uttampun62@gmail.com>
2023-04-04 08:42:31 +0545

    contact page done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Addtocart from './Components/Addtocart/Addtocart';
 import  './scss/main.scss';
 import Singup from './Components/Signup/Signup';
 import Login from './Components/Login/Login';
+import Contact from './Components/Contact/Contact';
+
+
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
@@ -15,6 +18,7 @@ function App() {
                      <Route exact path='/addtocart' element={ <Addtocart />}></Route>
                      <Route exact path='/signup' element={<Singup />}></Route>
                      <Route exact path='/login' element={<Login />}></Route>
+                     <Route exact path='/contact' element={<Contact />}></Route>
                   </Routes>     
              </div>  
     </BrowserRouter>
